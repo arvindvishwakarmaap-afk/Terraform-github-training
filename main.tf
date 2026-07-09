05a07c9954a5ef07a5b7a97ca39f4a59ea6eca0f
Arvind Vishwakarma <arvind.vishwakarmaap@gmail.com>
2026-07-09 21:27:16 +0530

    next

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+  features {}
+}   
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-network"
+  location = "Central India"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-terraform"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-vm"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
